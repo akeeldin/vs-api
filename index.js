e0c19c4e837137a6abd60d1fe818a0eddfb381c7
Akeel Din <adin@us.ibm.com>
2017-10-20 12:16:06 -0500

    empid GET param

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 'use strict';
-var fs = require('fs');
+var fs = require('fs');    //local filesystem
 
- exports.get = function(event, context) {
-   var contents = fs.readFileSync("public/index.html");
-   context.succeed({
-     statusCode: 200,
-     body: "Hello World",
-     headers: {'Content-Type': 'text/html'}
-   });
- };
+exports.get = function(event, context) {
+	var contents = fs.readFileSync("public/index.html");
 
+	var empid = event.params.querystring.empid;
+
+	context.succeed({
+		statusCode: 200,
+		body: "employee id : "+empid,
+		headers: {'Content-Type': 'text/html'}
+	});
+};
 
