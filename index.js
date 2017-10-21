7c96d7bda7fd6a21d105a4ed735459c47618247c
Akeel Din <adin@us.ibm.com>
2017-10-21 15:05:09 -0500

    fixed some remnents from 'product' codebase

--- index.js
+++ index.js
@@ -20,24 +20,17 @@ exports.get = function(event, context) {
         }
     }
 
-    let id = event.pathParameters.product || false;
-    switch(event.httpMethod) {
-
-        case "GET":
-            empid += " Its a GET id:"+id;
-
-           if(id) {
 
                 // Create DynamoDB service object
                 var ddb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
 
-                console.log("Querying for movies from 1992 - titles A-L, with genres and lead actor");
+                console.log("Querying for movies from Employee Db");
 //-----
 
-                AWS.config.update({
-                  region: "us-west-2",
-                  //endpoint: "http://localhost:8000"
-                });
+                // AWS.config.update({
+                //   region: "us-west-2",
+                //   //endpoint: "http://localhost:8000"
+                // });
 
                 var docClient = new AWS.DynamoDB.DocumentClient()
 
@@ -60,62 +53,105 @@ exports.get = function(event, context) {
                         console.log("GetItem succeeded:", JSON.stringify(data, null, 2));
                     }
                 });
-//-----
-
-                // var params = {
-                //     TableName : "employee",
-                //     ProjectionExpression:"#eid, name, status",
-                //     KeyConditionExpression: "#eid = 1",
-                //     ExpressionAttributeNames:{
-                //         "#eid": "emp_id"
-                //     }
-                //     // ExpressionAttributeValues: {
-                //     //     ":eidval":1
-                //     // }
-                // };
-
-                // console.log("About to run ddb query .  .  .");
-                // ddb.query(params, function(err, data) {
-                //   if (err) {
-                //     console.log("Error", err);
-                //   } else {
-                //     console.log("DynamoDB query didn't");
-                //     data.Items.forEach(function(element, index, array) {
-                //         emp_name = element.name.S
-                //         emp_status = element.status.S
-                //       console.log(element.name.S + " (" + element.status.S + ")");
-                //     });
-                //   }
-                // });
 
 
 
+//     let id = event.pathParameters.product || false;
+//     switch(event.httpMethod) {
 
-            //callback(null, {body: "This is a READ operation on emp_id " + id + "name : " + name + " status : " + status});
-                //return;  
-           } 
+//         case "GET":
+//             empid += " Its a GET id:"+id;
 
-        //     callback(null, {body: "This is a LIST operation, return all products"});
-        //     break;
+//            if(id) {
 
-        // case "POST":            
-        //     callback(null, {body: "This is a CREATE operation"}); 
-        //     break;
+//                 // Create DynamoDB service object
+//                 var ddb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
 
-        // case "PUT": 
-        //     callback(null, {body: "This is an UPDATE operation on product ID " + id});
-        //     break;
+//                 console.log("Querying for movies from 1992 - titles A-L, with genres and lead actor");
+// //-----
 
-        // case "DELETE": 
-        //     callback(null, {body:"This is a DELETE operation on product ID " + id});
-        //     break;
+//                 AWS.config.update({
+//                   region: "us-west-2",
+//                   //endpoint: "http://localhost:8000"
+//                 });
 
-        // default:
-        //     // Send HTTP 501: Not Implemented
-        //     console.log("Error: unsupported HTTP method (" + event.httpMethod + ")");
-        //     callback(null, { statusCode: 501 })
+//                 var docClient = new AWS.DynamoDB.DocumentClient()
 
-    }
+//                 var table = "employee";
+
+//                 var eid = 1;
+//                 //var title = "The Big New Movie";
+
+//                 var params = {
+//                     TableName: table,
+//                     Key:{
+//                         "emp_id": eid
+//                     }
+//                 };
+
+//                 docClient.get(params, function(err, data) {
+//                     if (err) {
+//                         console.error("Unable to read item. Error JSON:", JSON.stringify(err, null, 2));
+//                     } else {
+//                         console.log("GetItem succeeded:", JSON.stringify(data, null, 2));
+//                     }
+//                 });
+// //-----
+
+//                 // var params = {
+//                 //     TableName : "employee",
+//                 //     ProjectionExpression:"#eid, name, status",
+//                 //     KeyConditionExpression: "#eid = 1",
+//                 //     ExpressionAttributeNames:{
+//                 //         "#eid": "emp_id"
+//                 //     }
+//                 //     // ExpressionAttributeValues: {
+//                 //     //     ":eidval":1
+//                 //     // }
+//                 // };
+
+//                 // console.log("About to run ddb query .  .  .");
+//                 // ddb.query(params, function(err, data) {
+//                 //   if (err) {
+//                 //     console.log("Error", err);
+//                 //   } else {
+//                 //     console.log("DynamoDB query didn't");
+//                 //     data.Items.forEach(function(element, index, array) {
+//                 //         emp_name = element.name.S
+//                 //         emp_status = element.status.S
+//                 //       console.log(element.name.S + " (" + element.status.S + ")");
+//                 //     });
+//                 //   }
+//                 // });
+
+
+
+
+//             //callback(null, {body: "This is a READ operation on emp_id " + id + "name : " + name + " status : " + status});
+//                 //return;  
+//            } 
+
+//         //     callback(null, {body: "This is a LIST operation, return all products"});
+//         //     break;
+
+//         // case "POST":            
+//         //     callback(null, {body: "This is a CREATE operation"}); 
+//         //     break;
+
+//         // case "PUT": 
+//         //     callback(null, {body: "This is an UPDATE operation on product ID " + id});
+//         //     break;
+
+//         // case "DELETE": 
+//         //     callback(null, {body:"This is a DELETE operation on product ID " + id});
+//         //     break;
+
+//         // default:
+//         //     // Send HTTP 501: Not Implemented
+//         //     console.log("Error: unsupported HTTP method (" + event.httpMethod + ")");
+//         //     callback(null, { statusCode: 501 })
+
+//     }
 
     context.succeed({
      statusCode: 200,
