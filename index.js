30fe9b9b344a464c138e7ba48f2afa247d5065ef
Akeel Din <adin@us.ibm.com>
2017-10-21 15:41:07 -0500

    new sample

--- index.js
+++ index.js
@@ -25,50 +25,67 @@ exports.get = function(event, context) {
                 var ddb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
 
                 console.log("Querying for movies from Employee Db");
-//-----
 
-                // AWS.config.update({
-                //   region: "us-west-2",
-                //   //endpoint: "http://localhost:8000"
-                // });
+    var dynamodb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
+    var tableName = "employee";
+    dynamodb.scan({
+        TableName : tableName,
+        Limit : 10
+    }, function(err, data) {
+        if (err) {
+            context.done('error','reading dynamodb failed: '+err);
+        }
+        for (var i in data.Items) {
+            i = data.Items[i];
+            console.log(i.emp_id.S + ': '+ i.name.S);
+            context.done(null, "Ciao!");
+        }
+    });
 
-                var docClient = new AWS.DynamoDB.DocumentClient()
-
-                var table = "employee";
-
-                var eid = 1;
-                //var title = "The Big New Movie";
-
-                var params = {
-                    TableName: table,
-                    Key:{
-                        "emp_id": eid
-                    }
-                };
-
-                docClient.get(params, function(err, data) {
-                    if (err) {
-                        console.error("Unable to read item. Error JSON:", JSON.stringify(err, null, 2));
-                    } else {
-                        console.log("GetItem succeeded:", JSON.stringify(data, null, 2));
-                    }
-                });
-
-
-
-//     let id = event.pathParameters.product || false;
-//     switch(event.httpMethod) {
-
-//         case "GET":
-//             empid += " Its a GET id:"+id;
-
-//            if(id) {
-
-//                 // Create DynamoDB service object
-//                 var ddb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
-
-//                 console.log("Querying for movies from 1992 - titles A-L, with genres and lead actor");
 // //-----
+
+//                 // AWS.config.update({
+//                 //   region: "us-west-2",
+//                 //   //endpoint: "http://localhost:8000"
+//                 // });
+
+//                 var docClient = new AWS.DynamoDB.DocumentClient()
+
+//                 var table = "employee";
+
+//                 var eid = 1;
+//                 //var title = "The Big New Movie";
+
+//                 var params = {
+//                     TableName: table,
+//                     Key:{
+//                         "emp_id": eid
+//                     }
+//                 };
+
+//                 docClient.get(params, function(err, data) {
+//                     if (err) {
+//                         console.error("Unable to read item. Error JSON:", JSON.stringify(err, null, 2));
+//                     } else {
+//                         console.log("GetItem succeeded:", JSON.stringify(data, null, 2));
+//                     }
+//                 });
+
+
+
+// //     let id = event.pathParameters.product || false;
+// //     switch(event.httpMethod) {
+
+// //         case "GET":
+// //             empid += " Its a GET id:"+id;
+
+// //            if(id) {
+
+// //                 // Create DynamoDB service object
+// //                 var ddb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
+
+// //                 console.log("Querying for movies from 1992 - titles A-L, with genres and lead actor");
+// // //-----
 
 //                 AWS.config.update({
 //                   region: "us-west-2",
